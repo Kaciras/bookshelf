Kaciras/bookshelf

commit bfe33356788030589f77ba4d93e6450299fa7fbc

polish

rollup/html.js
@@ -5,18 +5,14 @@ import { minify } from "html-minifier-terser";
 import HtmlParser from "node-html-parser";
 
 /**
- * 构造一个 JS 模块，导入所有 ID 。
- * 没有用 reduce 因为参数是 Iterable，当然写成 [].reduce.call() 也行但不好看。
+ * Create a JavaScript code contains imports of IDs;
  *
- * @param ids 要导入的 ID 列表
- * @return {string} JS 代码
+ * @param ids {Iterable<string>} ID list to import.
+ * @return {string} JavaScript code.
  */
 export function dummyImportEntry(ids) {
-	let code = "";
-	for (const id of ids) {
-		code += `import "${id}"\n`;
-	}
-	return code;
+	return Array.prototype.reduce
+		.call(ids, (code, id) => code + `import "${id}"\n`, "");
 }
 
 export const minifyOptions = {
@@ -27,13 +23,9 @@ export const minifyOptions = {
 	removeAttributeQuotes: true,
 };
 
-// 绝对路径和找不到文件的不处理
+// Only process existing files with relative paths.
 function check(importer, url) {
-	if (url.charCodeAt(0) === 0x2F) {
-		return false;
-	}
-	const file = resolve(dirname(importer), url);
-	return existsSync(file);
+	return url.charCodeAt(0) !== 0x2F && existsSync(resolve(dirname(importer), url));
 }
 
 /**
@@ -43,12 +35,6 @@ function check(importer, url) {
  */
 export default function htmlPlugin() {
 	const documents = new Map();
-
-	/*
-	 * Rollup 的构建分为两个阶段：Build 和 Output Generation。
-	 * Build 加载并独立地处理每个模块，该阶段完成后应用已经可用。
-	 * Output Generation 打包模块、执行后期优化以及生成文件。
-	 */
 
 	return {
 		name: "html-entry",

chrome/userChrome.css
@@ -1,6 +1,7 @@
 /*
  * https://www.userchrome.org/
- * Copy this stylesheet to <profile>/chrome folder to restore the old tab style.
+
+ * Copy this file to <profile>/chrome folder to restore the legacy firefox tab style.
  */
 
 :root {

components/SearchBox.js
@@ -94,11 +94,6 @@ class SearchBoxElement extends HTMLElement {
 		this.fetcher.threshold = value;
 	}
 
-	/*
-	 * 对获取建议的中断分为两个阶段，先是防抖，一旦开始请求则不再受防抖的影响，
-	 * 只有下一次的请求才能中断前面的。
-	 * 这样的设计使得输入中途也能显示建议，并尽可能地减少了请求，与其他平台一致。
-	 */
 	handleInput(event) {
 		if (this.waitIME && event.isComposing) {
 			return;
@@ -150,10 +145,10 @@ class SearchBoxElement extends HTMLElement {
 	}
 
 	/**
-	 * 按回车键跳转到搜索页面，同时处理了输入法的问题。
+	 * Press the Enter key to jump to the search page, and deal with the IME。
 	 *
-	 * 由于 compositionend 先于 KeyUp 所以只能用 KeyDown 确保能获取输入状态。
-	 * Google 的搜索页面也是在 KeyDown 阶段就触发。
+	 * Since compositionend precedes KeyUp, only KeyDown can be used
+	 * to ensure `isComposing` is set. Google search also uses the KeyDown event.
 	 */
 	handleInputKeyDown(event) {
 		if (event.key !== "Enter") {

components/TaskButton.css
@@ -9,7 +9,7 @@ $time: 0.8s;
  */
 .dot-flashing {
 
-	/* 颜色属性使用 CSS 变量以便外部能够修改 */
+	/* Use CSS variables for external modification */
 	--color: #0765e0;
 
 	&::before, &::after {

components/TaskButton.js
@@ -10,14 +10,21 @@ template.innerHTML = `
 `;
 
 /**
- * 拥有繁忙状态的按钮，点击时启动任务并转为繁忙状态，完成后变为原样。
- * 繁忙状态保持原来的大小，内容变为加载指示器，点击繁忙状态的按钮会取消当前任务。
+ * A button with a busy state, click it starts the task and turns
+ * to a busy state, and changes back the task done.
  *
- * 【实现注意】
- * Firefox 仅支持的 Custom Element v1 不能够继承 HTMLElement 以外的元素，
- * 如果这么做会显示不出 ShadowDOM。
+ * When busy, clicking the button will abort the current task with AbortSignal.
+ *
+ * <h2>Why not extends HTMLButtonElement</h2>
+ * Firefox doesn't support extends elements other than HTMLElement.
+ * If you do this, the ShadowDOM will not be displayed.
  */
 class TaskButtonElement extends HTMLElement {
+
+	/**
+	 * Click handler returns a Promise for long running task.
+	 */
+	taskFn;
 
 	running = false;
 	controller = new AbortController();
@@ -31,10 +38,6 @@ class TaskButtonElement extends HTMLElement {
 		this.loadingEl = root.querySelector(".dot-flashing");
 		this.loadingEl.remove();
 
-		/*
-		 * ShadowRoot 是内部元素的大小（不含 :host），小于整个按钮，所以要监听外层。
-		 * 宿主元素和 ShadowRoot 的关系就像 html 与 body 一样。
-		 */
 		this.addEventListener("click", this.handleClick);
 	}
 
@@ -45,7 +48,7 @@ class TaskButtonElement extends HTMLElement {
 	async handleClick() {
 		const { running, taskFn } = this;
 
-		// 没有设置任务回调则与普通按钮一样
+		// Just like a normal button if no taskFn.
 		if (!taskFn) return;
 
 		if (!running) {
@@ -60,7 +63,7 @@ class TaskButtonElement extends HTMLElement {
 		const { slotEl, loadingEl, taskFn, style } = this;
 		this.running = true;
 
-		// 固定大小，防止内容切换后影响布局
+		// Prevent size changing after content switching.
 		const { width, height } = this.getBoundingClientRect();
 		style.width = width + "px";
 		style.height = height + "px";

components/TopSiteDialog.css
@@ -36,7 +36,6 @@ img {
 	border-radius: 4px;
 }
 
-/** 窗口足够宽，更长的就不显示了 */
 span {
 	white-space: nowrap;
 	overflow: hidden;

components/TopSiteDialog.js
@@ -46,11 +46,10 @@ itemTemplate.innerHTML = `
 `;
 
 /**
- * 导入对话框，能够读取浏览器原生的新标签页里的快捷方式，并将其添加到本插件中。
+ * Add shortcuts from browser builtin navigation page.
  *
- * 【获取数据的 API】
- * browser.topSites 仅支持读取，而新标签页却需要自定义快捷方式，
- * 所以只能从 topSites 导入然后保存到本插件的存储。
+ * <h2>API Note</h2>
+ * browser.topSites is readonly，so we need implement our shortcut storage，
  */
 class TopSiteDialogElement extends HTMLElement {
 

rollup.config.js
@@ -28,7 +28,6 @@ export default {
 
 	output: {
 		generatedCode: "es2015",
-		format: "esm",
 		dir: "dist",
 		chunkFileNames: "[name].js",
 	},

commit 0df2a148ddd8d841ffb5283e15fa74fa45de5083

fix regression introduced in bfe33356

rollup/html.js
@@ -10,9 +10,11 @@ import HtmlParser from "node-html-parser";
  * @param ids {Iterable<string>} ID list to import.
  * @return {string} JavaScript code.
  */
-export function dummyImportEntry(ids) {
-	return Array.prototype.reduce
-		.call(ids, (code, id) => code + `import "${id}"\n`, "");
+export function jsImports(ids) {
+	let code = "";
+	for (const id of ids)
+		code += `import "${id}"\n`;
+	return code;
 }
 
 export const minifyOptions = {