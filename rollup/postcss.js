611d70b0333d6b6dca400e7e96e51c130ac670b0
Kaciras <Kaciras@protonmail.com>
2023-01-16 12:06:06 +0800

    add postcss-calc

--- rollup/postcss.js
+++ rollup/postcss.js
@@ -2,23 +2,21 @@ import { env } from "node:process";
 import postcss from "postcss";
 import nested from "postcss-nested";
 import csso from "postcss-csso";
+import calc from "postcss-calc";
 import vars from "postcss-simple-vars";
 import varCompress from "postcss-variable-compress";
 
-/*
- * 【关于 import 的处理】
- * CSS 里的导入与 JS 不同，它是相对于文档而不是当前脚本，
- * 要正确的处理必须在构建期获取打包期的路径，很麻烦。
- */
+// Terser will try to eliminate escapes of strings, we don't need to care about it.
 
 const plugins = [
 	vars(),		// Support SCSS-style variables.
-	csso(),		// Compress output.
 	nested(),	// Support nesting.
+	calc(),		// Reduce calc() references.
 ];
 
 if (env.NODE_ENV === "production") {
-	plugins.push(varCompress()); // Minimum variable names.
+	plugins.push(csso());			// Compress output.
+	plugins.push(varCompress());	// Minimum variable names.
 }
 
 const convertor = postcss(plugins);
